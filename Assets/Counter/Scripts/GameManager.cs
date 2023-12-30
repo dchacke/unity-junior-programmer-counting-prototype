c826b4b33b1ee0b959648f142cb6d395bdece960
Dennis Hackethal <dh@dennishackethal.com>
2023-12-29 21:33:48 -0700

    Implement a game manager that spawns one sphere per second

---
+++ Assets/Counter/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject spherePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnSphere", 0, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnSphere()
+    {
+        Instantiate(spherePrefab, new Vector3(Random.Range(-10.0f, 10.0f), 10, 0), spherePrefab.transform.rotation);
+    }
+}
